fa46a6b35f9048f942f737cf729c44a011b9d17f
Plushveil <plushveil@gray.blue>
2026-01-03 18:24:33 +0100

    feat: enhance database connection logic

--- src/database.ts
+++ src/database.ts
@@ -24,6 +24,12 @@ const pool = await executeDatabaseSetup()
 
 export default pool
 
+const c = await pool.connect()
+console.log((await c.sql.now()).rows)
+
+c.release()
+pool.end()
+
 /**
  * Runs all migrations.
  */
@@ -108,7 +114,8 @@ async function executeDatabaseSetup (SUPABASE_PROJECT_REF?: string, SUPABASE_PAS
  *
  */
 export async function connect (connect?: Pool['connect']) : Promise<PoolClient & { sql: ReturnType<typeof getClientQueries> }> {
-  const client = (connect ? await connect() : await pool.connect()) as PoolClient & { sql: ReturnType<typeof getClientQueries> }
+  if (!connect) return await pool.connect()
+  const client = await connect() as PoolClient & { sql: ReturnType<typeof getClientQueries> }
   client.sql = client.sql || getClientQueries(client, getAllQueries(path.join(__root, 'database', 'sql')))
   return client
 }
